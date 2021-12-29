aad8d43188d5c2f222f6c23c9d0a61ec0daea358
Mike Lucent <mike.j.lucent@gmail.com>
2021-12-28 16:13:36 -0800

    corrected typos

--- main.py
+++ main.py
@@ -6,9 +6,9 @@ from reports import PdfReport
 budget1 = Budget(period = input("Enter the budget period (ex: January 2021): "))
 
 user1 = User(user_name= input("Enter your Name: "), income= float(input("Enter your yout total monthly income: ")), \
-needs=float(input("Enter your total monthly Needs (Bills, gas, groceries, personal care): ")), \
-wants =float(input("Enter your total monthly Wants (Clothing, dining out, gifts: ")), \
-savings = float(input("Enter your total monthly Savings/Debts (LT Savins, ST Savings, Credid Cards): ")))
+needs=float(input("Enter your total monthly Needs (Bills, Gas, Groceries, Personal Care): ")), \
+wants =float(input("Enter your total monthly Wants (Clothing, Dining Out, Gifts: ")), \
+savings = float(input("Enter your total monthly Savings/Debts (LT Savins, ST Savings, Credit Cards): ")))
 
 report1=PdfReport(budget1.period)
 report1.generate(user1, budget1)
